85ae47e776a4866c6b45c394e2cdc1faa5b05f85
truongzodinet <truong.tran@zodinet.tech>
2022-08-18 18:04:10 +0700

    feat: add upload file demo

--- pages/chat/[id].tsx
+++ pages/chat/[id].tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState, useRef } from 'react';
+import { Popover } from '@headlessui/react';
 import { NextPageWithLayout } from '../../types/global';
 import { useRouter } from 'next/router';
 import Title from '../../components/Home/Title';
@@ -13,6 +14,7 @@ import SendIcon from '../../components/Icons/SendIcon';
 import IconSetIcon from '../../components/Icons/chat/IconSetIcon';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import MessageItem from '../../components/Chat/MessageItem';
+import { BsImage } from 'react-icons/bs';
 export enum EMessageType {
     TEXT = 'text',
     IMAGE = 'image',
@@ -29,7 +31,7 @@ const messages = [
         receiverId: 2,
         message: [
             {
-                value: 'Hello',
+                value: 'Chào iem, c đứng đây từ chiều🥰', //Hello
                 type: EMessageType.TEXT,
             },
         ],
@@ -40,7 +42,7 @@ const messages = [
         receiverId: 1,
         message: [
             {
-                value: 'Nice to meet you',
+                value: 'Hello chị Thảo 💖',
                 type: EMessageType.TEXT,
             },
         ],
@@ -51,7 +53,7 @@ const messages = [
         receiverId: 1,
         message: [
             {
-                value: '/assets/images/favoriteImage1.png',
+                value: '/assets/images/temp.png', //favoriteImage1.png
                 type: EMessageType.IMAGE,
             },
         ],
@@ -125,6 +127,8 @@ const messages = [
 ];
 
 const Room: NextPageWithLayout = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [files, setFiles] = useState<File[]>([]);
     const router = useRouter();
     const { userId } = router.query;
 
@@ -136,10 +140,32 @@ const Room: NextPageWithLayout = () => {
         router.push(APP_PATH.PROFILE);
     };
 
+    const handleUpload = () => {
+        const input = inputRef.current;
+        if (input) {
+            input.click();
+        }
+    };
+    const handleChangeUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const images = e.target.files;
+        if (images) {
+            setFiles([...files, ...images]);
+        }
+    };
     return (
-        <section className="container relative">
+        <section className="container relative flex flex-col">
+            <input
+                multiple
+                className="hidden"
+                type="file"
+                name="file"
+                id="file"
+                onChange={handleChangeUpload}
+                ref={inputRef}
+                accept="image/*"
+            />
             <Title
-                className="py-0.5"
+                className="py-0.5 flex-shrink-0"
                 content={
                     <div className="justify-between flex-center-y">
                         <div className="p-2 cursor-pointer" onClick={onBack}>
@@ -151,20 +177,20 @@ const Room: NextPageWithLayout = () => {
                     </div>
                 }
             />
-            <div className="flex items-center justify-between py-4">
+            <div className="flex items-center justify-between flex-shrink-0 py-4">
                 <div className="relative flex justify-between">
                     <LovedStatusIcon className="absolute bottom-0 z-20 left-9" />
                     <div className="w-12 h-12 mr-5 image-container rounded-2xl">
                         <Image className="image" src="/assets/images/chat_avatar.png" alt="jdiwed" layout="fill" />
                     </div>
                     <div className="flex flex-col">
-                        <p className="font-bold body-1 text-neutral-100">Cameron Greer, 1</p>
+                        <p className="font-bold body-1 text-neutral-100">Chị Thảo, HR</p>
                         <span className="text-xs opacity-50">Cô bé ngây thơ</span>
                     </div>
                 </div>
                 <RightArrowIcon onClick={visitProfile} />
             </div>
-            <div className="flex gap-4 h-[calc(100vh-208px)] flex-col-reverse overflow-auto px-4">
+            <div className=" h-[calc(100vh-208px)] flex gap-4 flex-col-reverse overflow-auto px-4 py-2">
                 {messages.map((message, index) => (
                     <MessageItem key={index} isMe={message.receiverId === 1} messages={message.message} />
                 ))}
@@ -188,8 +214,27 @@ const Room: NextPageWithLayout = () => {
             >
                 {items}
             </InfiniteScroll> */}
-            <div className="absolute bottom-0 left-0 flex items-center justify-between w-full gap-2 px-4 py-6">
-                <PlusIcon />
+            <div className="flex gap-4 overflow-auto">
+                {files.map((file, index) => (
+                    <div className="flex-shrink-0 h-10 image-container" key={index}>
+                        <Image className="image" src={URL.createObjectURL(file)} alt="demo_img" layout="fill" />
+                    </div>
+                ))}
+            </div>
+            <div className="flex items-center justify-between flex-shrink-0 w-full gap-2 py-6 mt-auto">
+                <Popover className="relative">
+                    <Popover.Button>
+                        <PlusIcon />
+                    </Popover.Button>
+
+                    <Popover.Panel className="absolute left-0 z-10 bottom-[calc(100%+1rem)]">
+                        <div className="gap-2 p-2 bg-white rounded-md shadow-md flex-center-y">
+                            <Popover.Button className="p-2 border rounded-full border-slate-200" onClick={handleUpload}>
+                                <BsImage />
+                            </Popover.Button>
+                        </div>
+                    </Popover.Panel>
+                </Popover>
                 <div className="w-[267px] h-9 bg-neutral-5 rounded-3xl py-2 px-2 body-2 flex justify-between items-center">
                     <input type="text" className="bg-neutral-5 body-2 px-2 w-[94%] h-full" autoFocus placeholder="Aa" />
                     <IconSetIcon />
