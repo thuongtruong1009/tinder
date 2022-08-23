8ad7676b3231a22b4d17641f3582e489f02eb13a
truongzodinet <truong.tran@zodinet.tech>
2022-08-23 16:54:43 +0700

    style: logic display empty match

--- pages/match.tsx
+++ pages/match.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { MdOutlineTravelExplore } from 'react-icons/md';
 import React, { Fragment, useState } from 'react';
 import CloseIcon from '../components/Icons/CloseIcon';
 import MatchTitleIcon from '../components/Icons/match/MathTitleIcon';
@@ -9,6 +10,7 @@ import { Popover } from '@headlessui/react';
 import { NextPageWithLayout } from '../types/global';
 import BellIcon from '../components/Icons/BellIcon';
 import dynamic from 'next/dynamic';
+import APP_PATH from '../constant/appPath';
 
 const Portal = dynamic(() => import('../HOC/Portal'), { ssr: false });
 
@@ -36,12 +38,16 @@ const Matching: NextPageWithLayout = () => {
         const arr = [...matchedFriends];
 
         if (arr.length === 0) {
-            router.push('/chat');
+            router.push(APP_PATH.CHAT);
             return;
         }
 
         arr.shift();
         setMatchedFriends(arr);
+    };
+
+    const onFindOther = () => {
+        router.push(APP_PATH.SURF);
     };
     return (
         <Portal
@@ -49,39 +55,60 @@ const Matching: NextPageWithLayout = () => {
             className="fixed bottom-0 z-[1000] -translate-x-1/2 left-1/2 max-w-[377px] w-full "
             section="section"
         >
-            <section className="container flex flex-col justify-between text-white matchingFrame p-6.5 h-screen min-h-screen w-screen overflow-hidden overflow-y-auto bg-main-purple">
-                <a className="flex flex-row justify-end p-4 text-white cursor-pointer">
-                    <CloseIcon />
-                </a>
-                <HeartContainer matchedFriend={matchedFriends && matchedFriends[0]} />
-                <div className="flex-center flex-col gap-[1.6rem]">
-                    <MatchTitleIcon className="text-3xl match-title text-neutral-100" />
-                    <p className="text-base font-normal text-center">
-                        Đừng để người ấy phải đợi, <br />
-                        gửi lời chào ngay!
-                    </p>
-                </div>
-                <div className="w-full flex flex-col mb-4 gap-[2.2rem]">
-                    <form
-                        onSubmit={handleSend}
-                        className="relative flex flex-row justify-between w-full p-2.5 bg-white rounded-3xl text-main-purple"
-                    >
-                        <input
-                            className="w-[90%] px-2.5 text-base font-bold rounded-4xl placeholder:text-main-purple"
-                            type="text"
-                            onChange={handleInput}
-                            value={greetMessage}
-                            placeholder="Gửi lời chào"
-                        />
-                        <button type="submit" onClick={handleSend} className="bg-white">
-                            <SendIcon />
+            {matchedFriends.length > 0 && (
+                <section className="container flex flex-col justify-between text-white matchingFrame p-6.5 h-screen min-h-screen w-screen overflow-hidden overflow-y-auto bg-main-purple">
+                    <a className="flex flex-row justify-end p-4 text-white cursor-pointer">
+                        <CloseIcon />
+                    </a>
+
+                    <div className="flex flex-col gap-20">
+                        <HeartContainer matchedFriend={matchedFriends && matchedFriends[0]} />
+                        <div className="flex-center flex-col gap-[1.6rem]">
+                            <MatchTitleIcon className="text-3xl match-title text-neutral-100" />
+                            <p className="text-base font-normal text-center">
+                                Đừng để người ấy phải đợi, <br />
+                                gửi lời chào ngay!
+                            </p>
+                        </div>
+                        <div className="w-full flex flex-col mb-4 gap-[2.2rem]">
+                            <form
+                                onSubmit={handleSend}
+                                className="relative flex flex-row justify-between w-full p-2.5 bg-white rounded-3xl text-main-purple"
+                            >
+                                <input
+                                    className="w-[90%] px-2.5 text-base font-bold rounded-4xl placeholder:text-main-purple"
+                                    type="text"
+                                    onChange={handleInput}
+                                    value={greetMessage}
+                                    placeholder="Gửi lời chào"
+                                />
+                                <button type="submit" onClick={handleSend} className="bg-white">
+                                    <SendIcon />
+                                </button>
+                            </form>
+                            <button onClick={handleSkip} className="font-normal bg-main-purple">
+                                Skip
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            )}
+            {matchedFriends < 1 && (
+                <section className="container-np flex flex-col justify-between text-white p-6.5 h-screen min-h-screen w-screen overflow-hidden overflow-y-auto">
+                    <a className="flex flex-row justify-end p-4 text-neutral-80 cursor-pointer">
+                        <CloseIcon />
+                    </a>
+                    <div className="image-container">
+                        <Image className="image" src="/assets/images/empty_match.jpg" alt="empty_img" layout="fill" />
+                    </div>
+                    <div className="flex-center mb-4">
+                        <button className="bg-[#E56A4C] rounded-xl px-5 py-2 flex-center gap-2" onClick={onFindOther}>
+                            <MdOutlineTravelExplore />
+                            Discover new people
                         </button>
-                    </form>
-                    <button onClick={handleSkip} className="font-normal bg-main-purple">
-                        Skip
-                    </button>
-                </div>
-            </section>
+                    </div>
+                </section>
+            )}
         </Portal>
     );
 };
