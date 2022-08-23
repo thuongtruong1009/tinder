c289c360943f9ae5fff5f44f0ece59f3e9afa993
havaem <vhnvohoainam@gmail.com>
2022-08-23 14:12:28 +0700

    chore: fix data

--- app/store.ts
+++ app/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import user from '../redux/reducers/userSlice';
 import notification from '../redux/reducers/notificationSlice';
 import info from '../redux/reducers/infoSlice';
+import conversation from '../redux/reducers/conversationSlice';
 
 export const store = configureStore({
     reducer: {
         user,
         notification,
         info,
+        conversation,
     },
 });
 
