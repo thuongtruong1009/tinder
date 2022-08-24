aa3b3e6d228255b12b143d9c339950c976964e81
havaem <vhnvohoainam@gmail.com>
2022-08-24 09:35:23 +0700

    fix: style logo

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -5,8 +5,9 @@ interface Props {}
 export default function Loading(props: Props) {
     return (
         <section className="container relative">
-            <div className="absolute inset-0 bg-primary-50 flex-center">
-                <Logo className="font-extrabold leading-10 pointer-events-none select-none  text-h2" />
+            <div className="absolute inset-0 gap-4 bg-primary-50 flex-col-center">
+                <Logo />
+                <p className="font-extrabold leading-10 text-white pointer-events-none select-none text-h2">Foxy</p>
             </div>
         </section>
     );
