a81dbf313babb68df7bb2e2efd70ff0f0984ffc1
havaem <vhnvohoainam@gmail.com>
2022-08-26 09:39:35 +0700

    style: change style loading

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 export default function Loading(props: Props) {
     return (
-        <section className="container relative">
+        <section className="fixed inset-0">
             <div className="absolute inset-0 gap-4 bg-primary-50 flex-col-center">
                 <Logo />
                 <p className="font-extrabold leading-10 text-white pointer-events-none select-none text-h2">Foxy</p>
